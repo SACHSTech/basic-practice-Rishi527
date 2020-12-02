e21e1d711b0f52798c789015f275e7b7cd2a6fe5
Rishi Kalathil <rishi.kalathil23@ycdsbk12.ca>
2020-12-02 19:55:42 +0000

    practice 1 completed

---
+++ avg_marks.py
@@ -0,0 +1,10 @@
+print("hello")
+mark1 = float(input("Enter mark 1: "))
+mark2 = float(input("Enter mark 2: "))
+mark3 = float(input("Enter mark 3: "))
+mark4 = float(input("Enter mark 4: "))
+# compute the average of the 4 marks
+marks_avg = (mark1 + mark2 + mark3 + mark4)/4
+
+#output the average
+print("The average of the 4 marks is",marks_avg)
