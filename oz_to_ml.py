ff8c3722b2d09cd234b88ef30d07a9b60759cd4f
Rishi Kalathil <rishi.kalathil23@ycdsbk12.ca>
2020-12-02 20:12:49 +0000

    Completed Practice 2

---
+++ oz_to_ml.py
@@ -0,0 +1,4 @@
+ounces = float(input("Enter the amount of fluid in ounces"))
+servings = float(input("Enter the number of people to serve"))
+
+print("You will need ",(ounces * 29.5735296) * (servings/4), ("ml"))
